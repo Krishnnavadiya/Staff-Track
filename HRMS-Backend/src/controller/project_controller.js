e16ee2f121d6c67b7d6672e7fd839c83e23e650d
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-07-19 12:17:29 +0530

    final commit: finalize controllers with proper error handling and responses

---
+++ HRMS-Backend/src/controller/project_controller.js
@@ -0,0 +1,55 @@
+const ApiError = require("../utils/error")
+const ProjectModel = require("../model/project_model")
+const { projectValidation } = require("../config/joi.validation");
+const EmployeeModel = require("../model/employee_model");
+const { EMPLOYEE_ROLE } = require("../config/string");
+
+async function addProject(req, res, next) {
+    try {
+        req.body.companyId = req.id;
+        const proValid = projectValidation.validate(req.body)
+        if (proValid.error) {
+            return next(new ApiError(403, proValid.error.details[0].message))
+        }
+        const project = new ProjectModel(req.body);
+        await project.save();
+        res.status(201).json({ statusCode: 201, success: true, message: "project details added successfully" });
+    } catch (e) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+async function getProject(req, res, next) {
+    try {
+
+        let companyId = req.id;
+        if (req.role === EMPLOYEE_ROLE) {
+            companyId = await EmployeeModel.getCompanyId(req.id);
+        }
+        const projects = await ProjectModel.find({ companyId });
+        const workingProject = projects.filter((data) => data.isWorking == true)
+        res.status(200).json({ statusCode: 200, success: true, data: workingProject });
+    } catch (e) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+async function updateProject(req, res, next) {
+    try {
+        await ProjectModel.findByIdAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true });
+        res.status(200).json({ statusCode: 200, success: true, message: "project details update successfully" });
+    } catch (e) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+async function deleteProject(req, res, next) {
+    try {
+        await ProjectModel.findByIdAndUpdate({ _id: req.params.id }, { $set: { isWorking: false, status: "complete" } }, { new: true });
+        res.status(200).json({ statusCode: 200, success: true, message: "project delete sucessfully" });
+    } catch (error) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+module.exports = { addProject, getProject, updateProject, deleteProject };
---
+++ HRMS-Backend/src/controller/team_controller.js
@@ -0,0 +1,87 @@
+const ApiError = require("../utils/error")
+const TeamModel = require("../model/team_model")
+const { EMPLOYEE_ROLE } = require("../config/string");
+// const { teamValidation } = require("../config/joi.validation")
+
+async function addTeam(req, res, next) {
+    try {
+        req.body.companyId = req.id
+
+        // const teamValid = teamValidation.validate(req.body)
+        // if (teamValid.error) {
+        //     return next(new ApiError(403, teamValid.error.details[0].message))
+        // }
+
+        const team = new TeamModel(req.body)
+        await team.save()
+        res.status(201).json({ statusCode: 201, success: true, data: team, message: "team created successfully" })
+    } catch (e) {
+        next(new ApiError(400, e.message))
+    }
+}
+
+async function getTeam(req, res, next) {
+    try {
+      //  debugger;
+        if (req.role === EMPLOYEE_ROLE) {
+            const teams = await TeamModel.find({
+                $or: [
+                    { children: req.id },
+                    { "leader.leaderId": req.id }
+                ]
+            })
+            .populate("projectTitle")
+            .populate({
+                path: "leader.leaderId",
+                populate: [
+                    { path: "designation" },
+                    { path: "department" }
+                ]
+            })
+            .populate({
+                path: "leader.children",
+                populate:[ { path: "designation" } ,  { path: "department" }]
+            });
+        
+            const workingTeam = teams.filter((data) => data.isWorking === true);
+            res.status(200).json({ success: true, data: workingTeam });
+        }
+        else {
+            const teams = await TeamModel.find({ companyId: req.id })
+                .populate("projectTitle")
+                .populate({
+                    path: "leader.leaderId",
+                    populate:[ { path: "designation" } ,  { path: "department" }]
+                })
+                .populate({
+                    path: "leader.children",
+                    populate:[ { path: "designation" } ,  { path: "department" }]
+                });
+            
+            const workingTeam = teams.filter((data) => data.isWorking === true);
+            res.status(200).json({ success: true, data: workingTeam });
+        }
+    } catch (e) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+async function updateTeam(req, res, next) {
+    try {
+        const team = await TeamModel.findByIdAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true });
+        res.status(200).json({ statusCode: 200, success: true, data: team, message: "team details updated successfully" });
+    } catch (e) {
+        next(new ApiError(400, e.message))
+    }
+}
+
+async function deleteTeam(req, res, next) {
+    try {
+        await TeamModel.findByIdAndUpdate({ _id: req.params.id }, { $set: { isWorking: false } }, { new: true })
+        res.status(200).json({ statusCode: 200, success: true, message: "team delete sucessfully" })
+    } catch (e) {
+        next(new ApiError(400, e.message))
+    }
+}
+
+module.exports = { addTeam, getTeam, updateTeam, deleteTeam }
